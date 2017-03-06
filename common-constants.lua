101159dcda018a3ef96d16db99235f758faa78c0
Ole Johan Væringstad <ole.johan.varingstad@gmail.com>
2017-03-06 13:18:58 +0100

    Finish up TODOs

--- common-constants.lua
+++ common-constants.lua
@@ -1,20 +1,16 @@
 -- luacheck: globals CONKY_DEBUG
 local COMMON_NAME = "org.awesomewm"
-    --local COMMON_NAME = "org.naquadah.awesome"
 local CONKY_NAME = COMMON_NAME .. ".conky"
 local AWESOME_NAME = COMMON_NAME .. ".awful"
 
 local HOME = os.getenv("HOME")
 
--- TODO: make this a bit for flexible
 local CONKY_LAUNCH = HOME .. "/.config/awesome/conky/conky-awesome-launch"
 
 if CONKY_DEBUG then
     CONKY_NAME = "org.awesomewm.test.conky"
     CONKY_LAUNCH = CONKY_LAUNCH .. " --debug"
 end
-
--- todo: make this an option
 
 local constants = {
     CONKY_LAUNCH          = CONKY_LAUNCH,
